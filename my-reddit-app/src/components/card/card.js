7237f2c87eb940d578c0b91d137955a5f10936b0
Ainotna95 <a.mcdonnell123@gmail.com>
2024-06-07 20:32:15 +0100

    Adding url and images to post

--- my-reddit-app/src/components/card/card.js
+++ my-reddit-app/src/components/card/card.js
@@ -7,12 +7,19 @@ const Card = (props) => {
     return (
             <article className="reddit-post">
                 {/*Accessing title and image of post */}
+                <div>
                     <h3>{props.article.title}</h3>
+                </div>
+                {/*MAKE CODE SO THAT THE POST EITHER SHOWS AN IMAGE, LINK OR POST TEXT DEPENDING ON WHAT'S AVAILABLE */}
                     <div className="image">
-                    {props.article.preview ? <img src={props.article.url}/> : null}
-                    {props.article.preview  ? <a href={props.article.permalink}>Read more</a> : null}
-                    {props.article ? <p>{props.article.selftext}</p> : null}
+                {/*Accessing Reddit images*/}
+                    {props.article.url ?  <img src={props.article.url} alt={props.article.title}/> : null }
                     {/*{props.article.secure_media ? <video>{props.article.secure_media.reddit_video.fallback_url}</video> : null}*/}
+                    {props.article.url_overridden_by_dest ? <a href={props.article.url_overridden_by_dest}>{props.article.url_overridden_by_dest}</a> : null} 
+                    </div>
+                {/*Accessing Reddit post text */}
+                    <div className="post-text">
+                    {props.article.selftext ? <p>{props.article.selftext}</p> : null}
                     </div>
                     
 
