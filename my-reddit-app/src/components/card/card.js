28bb047ab1e0c3be5f0569615eb61c00321f9245
Ainotna95 <a.mcdonnell123@gmail.com>
2024-06-07 07:05:36 +0100

    Adding images, text and media to Reddit posts

--- my-reddit-app/src/components/card/card.js
+++ my-reddit-app/src/components/card/card.js
@@ -2,13 +2,16 @@ import "./Card.css";
 
 //pass through props to get our titles
 const Card = (props) => {
-        
+
+    //add alt text property to image tags
     return (
             <article className="reddit-post">
                 {/*Accessing title and image of post */}
                     <h3>{props.article.title}</h3>
                     <div className="image">
-                    {props.article.preview ? <img src={props.article.preview.images[0].source.url}/> : null}
+                    {props.article.preview ? <img src={props.article.url}/> : <img src={props.article.url_overridden_by_dest} />}
+                    {props.article ? <p>{props.article.selftext}</p> : null}
+                    <video>{props.article.secure_media.reddit_video.fallback_url}</video>
                     </div>
                     
 
