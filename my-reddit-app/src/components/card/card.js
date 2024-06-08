19ba520536f8969fc1d9a0b2fae3240b6ef86415
Ainotna95 <a.mcdonnell123@gmail.com>
2024-06-08 12:15:11 +0100

    Trying to make video show in Reddit app

--- my-reddit-app/src/components/card/card.js
+++ my-reddit-app/src/components/card/card.js
@@ -10,17 +10,17 @@ const Card = (props) => {
                 <div>
                     <h3>{ props.article.title }</h3>
                 </div>
+
                 {/*MAKE CODE SO THAT THE POST EITHER SHOWS AN IMAGE, LINK OR POST TEXT DEPENDING ON WHAT'S AVAILABLE */}
                     <div id="post-image">
                 {/*Accessing Reddit images - either located in a gallery or have a post_hint property*/}
                     { props.article.post_hint === "image" || props.article.is_gallery === true ?  <img src={props.article.thumbnail} alt={props.article.title}/> : null }
-                    
                     </div>
                     <div id="post-video">
                 {/*Accessing Reddit videos - offering alternative URL if the browser cannot load video*/}
                     { props.article.secure_media ? 
                         <video width="750" height="500" controls>
-                        <source src={props.article.secure_media.reddit_video.fallback_url} type="video/mp4" />
+                        <source src={props.article.url} />
                         <p>
                           Your browser doesn't support this video. Here is a
                           <a href={props.article.secure_media.reddit_video.fallback_url}>link to the video</a> instead.
@@ -29,9 +29,11 @@ const Card = (props) => {
                     </div>
                 {/*Accessing Reddit post text */}
                     <div id="post-text">
-                    { props.article.selftext ? <p>{props.article.selftext}</p> : null }
+                        { props.article.selftext ? <p>{props.article.selftext}</p> : null }
                     </div>
+                    <div id="post-read-more">
                     
+                    </div>
 
             </article>
     )
