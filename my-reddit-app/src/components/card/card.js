00c98f194df292e355f8f72699d5563d43347a3a
Ainotna95 <a.mcdonnell123@gmail.com>
2024-06-09 08:44:28 +0100

    Linking articles from external sources to Reddit

--- my-reddit-app/src/components/card/card.js
+++ my-reddit-app/src/components/card/card.js
@@ -14,25 +14,19 @@ const Card = (props) => {
                 {/*MAKE CODE SO THAT THE POST EITHER SHOWS AN IMAGE, LINK OR POST TEXT DEPENDING ON WHAT'S AVAILABLE */}
                     <div id="post-image">
                 {/*Accessing Reddit images - either located in a gallery or have a post_hint property*/}
-                    { props.article.post_hint === "image" || props.article.is_gallery === true ?  <img src={props.article.thumbnail} alt={props.article.title}/> : null }
+                    { props.article.post_hint === "image" || props.article.is_gallery === true ?  <img src={props.article.url} alt={props.article.title}/> : null }
                     </div>
                     <div id="post-video">
                 {/*Accessing Reddit videos - offering alternative URL if the browser cannot load video*/}
-                    { props.article.secure_media ? 
-                        <video width="750" height="500" controls>
-                        <source src={props.article.secure_media.reddit_video.fallback_url} />
-                        <p>
-                          Your browser doesn't support this video. Here is a
-                          <a href={props.article.secure_media.reddit_video.fallback_url}>link to the video</a> instead.
-                        </p>
-                      </video> : null }
+                    { props.article.secure_media ? <p><a href={props.article.url}>Link to the video</a></p> : null }
                     </div>
                 {/*Accessing Reddit post text */}
                     <div id="post-text">
                         { props.article.selftext ? <p>{props.article.selftext}</p> : null }
                     </div>
                     <div id="post-read-more">
-                    
+                {/*Accessing articles from external websites */}
+                    { props.article.domain.endsWith(".com") ? <p><a href={props.article.url}>Read more...</a></p> : null }
                     </div>
 
             </article>
