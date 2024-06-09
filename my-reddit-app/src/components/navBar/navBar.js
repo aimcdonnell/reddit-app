32fe2ac18a62e394266dba7c7520b4a70f8dc031
Ainotna95 <a.mcdonnell123@gmail.com>
2024-06-09 10:10:34 +0100

    Adding styling and starting to add functionality to search bar

--- my-reddit-app/src/components/navBar/navBar.js
+++ my-reddit-app/src/components/navBar/navBar.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import "../../App"
 import "./NavBar.css";
 
 //Add Reddit logo to NavBar
-const NavBar = () => {
+//Add search functionality to Reddit app
+const NavBar = (props) => {
 
     return(
+        <>
         <div className="nav-bar">
-            <h2 id="nav-title">Seddit</h2>
-            <input type="text" className="input" value="popular"/>
+            <h1 id="nav-title">Seddit</h1>
+            <div id="input-bar">
+                <input type="text" className="input" value={subreddit} onChange={e => setArticles(e.target.value)}/>
+            </div>
         </div>
+
+        </>
     )
 }
 
