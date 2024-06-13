dc7ef50d4c4c519a8bb78490c1fb964dc44eb964
Ainotna95 <a.mcdonnell123@gmail.com>
2024-06-13 07:07:20 +0100

    Fetching data for Articles and NavBar

--- my-reddit-app/src/components/navBar/navBar.js
+++ my-reddit-app/src/components/navBar/navBar.js
@@ -1,5 +1,5 @@
 import "./NavBar.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaReddit, FaSearch } from "react-icons/fa";
 
 //Add Reddit logo to NavBar
@@ -9,21 +9,6 @@ import { FaReddit, FaSearch } from "react-icons/fa";
 const NavBar = () => {
     const [input, setInput] = useState("");
     console.log(input);
-    
-    //we want to fetch data every time the user inputs some text
-    //the function takes in a value (the text to search for)
-    const fetchData = (value) => {
-        fetch("https://www.reddit.com/r/pics.json?limit=10").then((response) => response.json()).then(json => {
-            console.log(json);
-        });
-    };
-    
-    //handleChange updates the state for us
-    //whenever we make a change it will make a request to the fetchData API
-    const handleChange = (value) => {
-        setInput(value);
-        fetchData(value);
-    };
 
     return(
         <>
@@ -34,7 +19,7 @@ const NavBar = () => {
                 <FaSearch id="search-icon"/>
                 {/*onChange - Whenever the user changes the value inside input, take in the event and set the input variable
                  to the value inside the input element*/}
-                <input id="search-bar" placeholder="Type to search..." type="text" onChange={(e) => handleChange(e.target.value)} className="input" value={input}/>
+                <input id="search-bar" placeholder="Type to search..." type="text" onChange={(e) => setInput(e.target.value)} className="input" value={"r/pics"}/>
             </div>
         </div>
 
